544047ea934b0f36ed5cc99d10e0a4df54d606ba
magnus346 <theo@slynt.com>
2023-02-15 11:46:05 +0100

    'first'

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
 
+const tkn = 'BiVCAyuspuH3WNcyIYoRrfZwbNUiVZ4wTCpZy9Qh00s';
+const id = '9a35508a-c0fb-43ee-8728-57be7a4f0bb2';
+
 exports.handler = async event => {
 	const subject = event.queryStringParameters.name || 'World'
 	const ip = await axios({
