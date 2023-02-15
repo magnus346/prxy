c3adeb40a14933f75d7efa8251f4587cd11e4bc0
magnus346 <theo@slynt.com>
2023-02-15 11:30:03 +0100

    'first'

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,15 @@
+const axios = require('axios');
+
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}!`,
-  }
+	const subject = event.queryStringParameters.name || 'World'
+	const ip = await axios({
+		url: "https://ifconfig.me/",
+		method: "get"
+	}).catch(function (error) {
+		console.log(error.response);
+	});
+	return {
+		statusCode: 200,
+		body: `Hello ${ip.data}!`,
+	}
 }
